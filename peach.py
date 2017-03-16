9b3f2251620f4ed899b7d2c2e47a2e46fd44543f
Jacob <madridjacobr@gmail.com>
2017-03-16 10:29:16 -0700

    update. it works now.

--- peach.py
+++ peach.py
@@ -1,11 +1,15 @@
 import twitter
 import warnings
+import time
 
 warnings.filterwarnings("ignore")
 
 MY_CONSUMER_KEY = "Ap1ok8lEFoWkPc4vTlapwpahv"
+
 MY_CONSUMER_SECRET = "JoaUdPDaeFJA4CZCt5u5IvvmHpd2ltbtFXls7DwYUX7Qd7THgE"
+
 MY_ACCESS_TOKEN_KEY = "841890782227386369-hybolyBqDLaIBR8AKmD4ExNFkg6I8Od"
+
 MY_ACCESS_TOKEN_SECRET = "oBOsY2VgeZ6l1uJRTiQiIEO08lBN8SM33EUbfq0Qc2g3F"
 
 
@@ -16,6 +20,8 @@ api = twitter.Api(consumer_key=MY_CONSUMER_KEY,
                   access_token_key=MY_ACCESS_TOKEN_KEY,
                   access_token_secret=MY_ACCESS_TOKEN_SECRET,
                   sleep_on_rate_limit=True)
+
+#print api.VerifyCredentials()
 
 b = open('nazis.txt')
 b2 = b.read().decode("utf-8-sig").encode("utf-8")
@@ -28,24 +34,36 @@ for bad in bigbads:
     try:
         
         baddie = api.GetUser(screen_name=bad)
-        print "Blocking user " + baddie.screen_name
-        api.CreateBlock(screen_name = baddie.screen_name)
+        source = baddie.screen_name
+        print "Blocking user " + source
+        api.CreateBlock(screen_name = source)
+        print "Successfully blocked!"
+        badslist[baddie.id] = True
         try:
-            bad_follows = api.GetFollowers(screen_name = baddie.screen_name)
-            badslist[baddie.user_id] = True
-            for follow in bad_follows:
-                try:
-                    print "Checking status of user " + follow.screen_name + "..."
-                    if follow.id not in badslist:
-                        badslist[follow.id] = True
-                    else:
-                        print "Blocking user " + follow.screen_name
-                        api.CreateBlock(screen_name=follow.screen_name)
-                except:
-                    print "Something bad happened when we tried to process a follower."
+            bad_follows = api.GetFollowersPaged(screen_name = source)
+            while True:
+                next_cursor = bad_follows[0]
+                print "Retrieved a batch of " + str(len(bad_follows)) + " followers."
+                for follow in bad_follows[2]:
+                    try:
+                        #print "Checking status of user " + follow.screen_name + "..."
+                        if follow.id not in badslist:
+                            badslist[follow.id] = True
+                        else:
+                            print "Blocking user " + follow.screen_name
+                            api.CreateBlock(screen_name=follow.screen_name)
+                            print "Successfully blocked!"
+                    except:
+                        print "Something bad happened when we tried to process a follower."
+                if (next_cursor == 0):
+                    break
+                else:
+                    print "Sleeping..."
+                    time.sleep(10)
+                    bad_follows = api.GetFollowersPaged(screen_name = source, cursor=next_cursor)
+                
         except:
             print "We couldn't get the followers..."
         
     except:
         print "Error, we couldn't get user data for that baddie."
-        pass
